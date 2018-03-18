fd06d263634f6339ada6bdebcc39499c7161d73d
Mostafa <mwhassan@wisc.edu>
2018-03-18 11:07:52 -0500

    Completed PerformanceAnalysisHash, except commenting and cleanup.

--- src/PerformanceAnalysisHash.java
+++ src/PerformanceAnalysisHash.java
@@ -7,76 +7,179 @@ public class PerformanceAnalysisHash implements PerformanceAnalysis {
 	/************************
      *Constants
      ***********************/
-	private HashTable table; //hash table to be compared to TreeMap
-	private TreeMap treemap; //TreeMap
+	
+	private HashTable<Object, Object> table; //hash table to be compared to TreeMap
+	private TreeMap<Object, Object> treemap; //TreeMap
+	
+	 private File details; //details file, which lists other files
+	
     private ArrayList<String> inputData;//stores input from file
-    private File details; //details file, which lists other files
-    private BufferedReader br; //buffered reader for details file
+    
+    private String directory = "data/"; //directory in which detail file is
+    private String currFile; //current working file
     private String output; //report to be printed
     
     /************************
      *Constructors
      ***********************/
-    public PerformanceAnalysisHash(){
+    
+    public PerformanceAnalysisHash(String details_filename) throws IOException {
+    	//initialize structures
+    	table = new HashTable<Object, Object>();
+    	treemap = new TreeMap<Object, Object>();
+    	
+    	//header
     	output = "------------------------------------------------------------------------------------------------\r\n" +
     			"									Performance Analysis Report\r\n" + 
     			"------------------------------------------------------------------------------------------------\r\n" +
-    			"|            FileName|      Operation| Data Structure|   Time Taken (micro sec)|     Bytes Used|\r\n" +
+    			"|\t\tFileName|\t\tOperation|\t\tData Structure|\t\tTime Taken (micro sec)|\t\tBytes Used|\r\n" +
     			"------------------------------------------------------------------------------------------------";
-    }
-    public PerformanceAnalysisHash(String details_filename) {
-    	this();
+    	
+    	//open details file
     	details = new File(details_filename);
-    	try {
-        br = new BufferedReader(new FileReader(details));
-        br.readLine(); // discard first line of file
-        br.close();
-    	}catch (IOException e) {}
     }
+    
+    /************************
+     *Public Interface
+     ***********************/
+    
     @Override
     public void compareDataStructures() {
     	try{
-    		String currFile = br.readLine().split(",")[0];
+    		BufferedReader br = new BufferedReader(new FileReader(details));
+            br.readLine(); // discard first line of file
+    		
+    		String currLine = br.readLine();
+    		if(currLine != null) currFile = currLine.split(",")[0]; //get file name
+    		
 	        while(currFile != null) {
-	        	loadData(currFile);
+	        	//load this file
+	        	loadData(directory + currFile);
+	        	
+	        	//compare the structures
 	        	compareInsertion();
 	        	compareSearch();
 	        	compareDeletion();
-	        	currFile = br.readLine().split(",")[0];
+	        	
+	        	//go to next file
+	        	currLine = br.readLine();
+	        	if(currLine != null) currFile = currLine.split(",")[0];
+	        	else currFile = null;
 	        }
+	        br.close();
     	}catch(IOException e) {}
     }
 
     @Override
     public void printReport() {
-        File results = new File("results.txt");
+        File results = new File(directory + "results.txt");
         
         try {
         BufferedWriter bw = new BufferedWriter(new FileWriter(results));
         bw.write(output);
+        bw.close();
         }catch(IOException e) {}
     }
 
     @Override
     public void compareInsertion() {
     	long startTime, time, mem; //time and memory value storage variables
-    	startTime = System.currentTimeMillis();
+    	Runtime runtime = Runtime.getRuntime();
+    	
+    	//put into HashTable
+    	startTime = System.nanoTime();
         for(Object x : inputData) {
         	table.put(x, x);
         }
-        time = System.currentTimeMillis() - startTime;
-        //add comment
-        //TODO: finish this method
+        runtime.gc();
+        mem = runtime.totalMemory() - runtime.freeMemory();
+        time = (System.nanoTime() - startTime)/1000; //convert to microseconds
+        
+        print(currFile, "PUT", "HASHTABLE", time, mem);
+        table = new HashTable<Object, Object>(); //reset HashTable
+        
+        //put into TreeMap
+        startTime = System.nanoTime();
+        for(Object x : inputData) {
+        	treemap.put(x, x);
+        }
+        runtime.gc(); //should delete tree previously inserted into
+        mem = runtime.totalMemory() - runtime.freeMemory();
+        time = (System.nanoTime() - startTime)/1000; //convert to microseconds
+        
+        print(currFile, "PUT", "TREEMAP", time, mem);
+        treemap = new TreeMap<Object, Object>(); //reset TreeMap
+        
     }
 
-    @Override
+	@Override
     public void compareDeletion() {
-        //TODO: Complete this method
+		long startTime, time; //time value storage variables
+    	
+    	//put into HashTable
+        for(Object x : inputData) {
+        	table.put(x, x);
+        }
+        
+        startTime = System.nanoTime();
+        //remove from HashTable
+        for(Object x : inputData) {
+        	table.remove(x);
+        }
+        time = (System.nanoTime() - startTime)/1000; //convert to microseconds
+        
+        print(currFile, "REMOVE", "HASHTABLE", time, 0);
+        table = new HashTable<Object, Object>(); //reset HashTable
+        
+        //put into TreeMap
+        for(Object x : inputData) {
+        	treemap.put(x, x);
+        }
+        
+        startTime = System.nanoTime();
+        //remove from TreeMap
+        for(Object x : inputData) {
+        	treemap.remove(x);
+        }
+        time = (System.nanoTime() - startTime)/1000; //convert to microseconds
+        
+        print(currFile, "REMOVE", "TREEMAP", time, 0);
+        treemap = new TreeMap<Object, Object>(); //reset TreeMap
     }
 
     @Override
     public void compareSearch() {
-        //TODO: Complete this method
+    	long startTime, time; //time value storage variables
+    	
+    	//put into HashTable
+        for(Object x : inputData) {
+        	table.put(x, x);
+        }
+        
+        startTime = System.nanoTime();
+        //search for all elements in HashTable
+        for(Object x : inputData) {
+        	table.get(x);
+        }
+        time = (System.nanoTime() - startTime)/1000; //convert to microseconds
+        
+        print(currFile, "GET", "HASHTABLE", time, 0);
+        table = new HashTable<Object, Object>(); //reset HashTable
+        
+        //put into TreeMap
+        for(Object x : inputData) {
+        	treemap.put(x, x);
+        }
+        
+        startTime = System.nanoTime();
+        //search for all elements in TreeMap
+        for(Object x : inputData) {
+        	treemap.get(x);
+        }
+        time = (System.nanoTime() - startTime)/1000; //convert to microseconds
+        
+        print(currFile, "GET", "TREEMAP", time, 0);
+        treemap = new TreeMap<Object, Object>(); //reset TreeMap
     }
 
     /*
@@ -98,4 +201,12 @@ public class PerformanceAnalysisHash implements PerformanceAnalysis {
         }
         br.close();
     }
+    
+    /************************
+     *Private Helpers
+     ***********************/
+    
+    private void print(String file, String operation, String structure, long time, long mem) {
+		output += "\n|\t\t" + file + "|\t\t" + operation + "|\t\t" + structure + "|\t\t" + time + "|\t\t" + mem + "|";
+	}
 }
